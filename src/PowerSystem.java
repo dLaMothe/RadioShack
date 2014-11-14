edd3a836805e2a6b7e5e5fa1af0700009bfb1e6e
David Ar Lamothe <lamothda@cub10.csci.viu.ca>
2014-11-14 15:38:08 -0800

    Testing merging of branches

---
+++ src/PowerSystem.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invasionforce;
+
+/**
+ *
+ * @author Michael
+ */
+class PowerSystem {
+
+    PowerSystem(int missleCapasity) {
+        
+    }
+
+    PowerSystem(double MAX_POWER, int TRT_MISSLES, int ANTIMATTER_PODS) {
+        
+    }
+
+    PowerSystem(double MAX_POWER) {
+        
+    }
+    
+}
