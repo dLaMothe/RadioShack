8c3921fb43b0f2aaeb4ea038c7e3120cf9ea418c
Artur Sukhenko <unitedmm@gmail.com>
2014-11-14 16:24:51 -0800

    Change output message

--- arturSpace/src/GitTest.java
+++ arturSpace/src/GitTest.java
@@ -5,7 +5,8 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Git is fun");
+		System.out.println("Git is cool?");
+		//testing
 
 	}
 
