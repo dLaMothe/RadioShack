efaaf30eb93e5d7a5594b097a3ee38de952978b1
Michael Koonts <mk.77@live.ca>
2014-11-22 12:32:29 -0800

    Fixed a warning

--- src/gameObjects/CriticalPowerException.java
+++ src/gameObjects/CriticalPowerException.java
@@ -2,6 +2,11 @@ package gameObjects;
 
 
 public class CriticalPowerException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public CriticalPowerException() 
 	{
 		super();
