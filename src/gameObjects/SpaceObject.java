c3c626697b030ca756cdc0e8ab772ef68dc7b8bd
Michael Koonts <mk.77@live.ca>
2014-11-23 09:20:14 -0800

    Updated documentation on SpaceObject

--- src/gameObjects/SpaceObject.java
+++ src/gameObjects/SpaceObject.java
@@ -4,18 +4,19 @@ import board.Position;
 import board.Positionable;
 import board.Sector;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.util.Observable;
 
 /**
- *
+ * Overview: This class is intended to serve as a base class for all the space objects needed in
+ * this game.  It should serve as the top of the inheritance tree for:
+ * 								Player controlled - Ship
+ *  							Player produced - Weapons - all types
+ *  							Player friendly - Space Stations
+ * 								All active obstacles - Jovians - all types
+ *  							All inactive obstacles - Stars
+ *  This class provides the basic functionality that all the listed objects require to be represented 
+ *  in a meaningful way in the game.  It also extends Observable which provides an easy interface to the 
+ *  GUI to keep updated on state changes.					
  * @author Michael Koonts
  */
 
@@ -26,8 +27,10 @@ public class SpaceObject extends Observable implements Positionable{
     protected boolean detectable = false;
 
     /**
-     *
-     * @param p
+     * REQUIRES: @param sec - a valid board.Sector of which the inhabitant is either
+     * null or ok to overwrite
+     * MODIFIES: game state
+     * EFFECTS: returns a new object at the given sector
      */
     public SpaceObject(Sector sec){
         sector = sec;
@@ -35,29 +38,41 @@ public class SpaceObject extends Observable implements Positionable{
     }
     
     /**
-     *
-     * @return a valid position object
-     * 			Note: a null value indicates a problem with this
+     * REQUIRES: nothing
+     * MODIFIES: nothing
+     * EFFECTS: @return a valid board.Position object
+     * 			Note: a null value indicates a problem with either this
+     *			or the sector this is located in
      */
     @Override
     public Position getPosition(){
         return sector.getPosition();
     }
     
-    //REQUIRES: the caller to ensure that the given sector is an 
-    // appropriate place to set this object
-    //MODIFFIES: this, sec
-    //EFFECTS: sets this object in the sector provided
+    /**
+     * REQUIRES: the caller to ensure that the given sector is an 
+     * 			 appropriate place to set this object
+     * MODIFFIES: this and @param sec
+     * EFFECTS: sets this object in the sector provided
+     */
     public void setSector(Sector sec){
         sector = sec;
         sector.setInhabitant(this);
     }
-    
-    
+    /**
+     * REQUIRES: nothing
+     * MODIFIES: this
+     * EFFECTS: sets the detectable state of this to @param det
+     */
     public void setDetectable(boolean det){
         detectable = det;
     }
-    
+    /**
+     * REQUIRES: nothing
+     * MODIFIES: nothing
+     * EFFECTS: This method is intended for use with the short range sensors
+     * ie. if @returns is false this should show up as an 'X' on the display
+    */
     public boolean getDetectable(){
         return detectable;
     }
