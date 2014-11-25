58d392432f44ae4104ed16caf55457ee6540d2df
Vitalii Egorchatov <egorchatov@mail.ru>
2014-11-24 17:28:10 -0800

    added stuff

---
+++ src/gameObjects/Maser.java
@@ -0,0 +1,43 @@
+package invade.src.gameObjects;
+
+import invade.src.board.Sector;
+import java.util.ArrayList;
+
+public class Maser
+	extends Weapon{
+	
+	private ArrayList<Sector> sectors = null;
+	private Boolean hit = false;
+	
+	public Maser(int[] velocity){
+		super(velocity);
+		label = "~";
+		sectors = new ArrayList<Sector>();
+	}
+	
+	@Override
+	public void Move(){
+		Sector curSector = getCurSector();
+		Sector nextSector = getCurQuadrant().getNext(curSector, velocity[0]);	
+		sectors.add(curSector);
+		if(hit){
+			curSector.setInhabitant(new Void());
+			return;
+		}
+		if(nextSector == null || !(nextSector.getInhabitant() instanceof Void)){
+			hit = true;
+		}
+		if(nextSector != null){
+			nextSector.setInhabitant(this);
+		}
+	}
+	
+	public Sector getNext(){
+		if(sectors.size() == 0){
+			return null;
+		} 
+		Sector sector = sectors.get(0);
+		sectors.remove(0);
+		return sector;	
+	}
+}
---
+++ src/gameObjects/Void.java
@@ -0,0 +1,17 @@
+package invade.src.gameObjects;
+
+import invade.src.board.Position;
+
+
+public class Void extends SpaceObject{
+	
+	public Void(){
+		label = "";
+	}
+
+	@Override
+	public Position getPosition() {
+		// TODO Auto-generated method stub
+		return position;
+	}
+}
